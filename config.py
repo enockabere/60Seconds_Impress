886209f753124ffeb9f1bf01d8e453d161a32d18
enockabere <maebaenock95@gmail>
2021-08-23 14:31:59 +0300

    deployment updates

--- config.py
+++ config.py
@@ -4,18 +4,20 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY= os.environ.get('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://enock:Maeba1995@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://enock:Maeba1995@localhost/pitch'       
+    DEBUG = True    
 
 class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://enock:Maeba1995@localhost/pitch'       
+    # DEBUG = True  
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # or other relevant config var
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-    DEBUG = True
 
-class DevConfig(Config):
-        
-    DEBUG = True
+
 config_options = {
     'development': DevConfig,
     'production': ProdConfig
